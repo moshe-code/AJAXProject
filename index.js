9b6a9370ff31d8384dfeef78cb0c1cb051b6b7a1
Moshe Weiner <moshesweiner@gmail.com>
2022-01-11 12:13:31 +0200

    stytle

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
-$(function(){
+$( () => {
 
-
-
+const loadCoinData = () => {
+  for (i = 0;i < 20; i++){
+    let cardStr = `
+    <div class="card" style="width: 18rem;">
+    <div class="card-body">
+        <h5 class="card-title">Coin </h5>
+        <p class="card-text">Coin Name</p>
+        <a href="#" class="btn btn-primary">More Info</a>
+    </div>
     
-  
-  });
+    `
+  $('.card-body').append(cardStr)
+
+  }
+
+
+loadCoinData()
+}})
+
+
+$('#load-coins').click(function (e){
+  e.preventDefault()
+
+  $.ajax({
+    url: `https://api.coingecko.com/api/v3/coins/list`,
+    success: data =>{
+      console.table(data)
+    }
+  })
+})
