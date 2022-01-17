427ca620fe3643bdbc277fe0303605f6b6eecd71
Moshe Weiner <moshesweiner@gmail.com>
2022-01-17 17:21:23 +0200

    display cards

--- index.js
+++ index.js
@@ -1,32 +1,48 @@
-$( () => {
+$(() => {
 
-const loadCoinData = () => {
-  for (i = 0;i < 20; i++){
-    $(".card-container").append(`
-    <div class="card" style="width: 18rem;">
-    <div class="card-body">
-        <h5 class="card-title">${List[i].symbol}Coin </h5>
-        <p class="card-text">${List[i].name}Coin Name</p>
-        <a href="#" class="btn btn-primary">More Info</a>
-    </div>  
-    <div class="form-check form-switch" id="toggle-btn">
-    <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckChecked" checked>
-</div>
-</div>
-    `
-    )
+
+  $('.card-container').hide()
+ 
+  const loadCoinData = (list) => {
+  for (i = 0; i < 100; i++) {
+    $("#cards-container").append(`
+    <div class="col-sm-4">
+      <div class="card" style="width: 18rem;">
+      <div class="card-body">
+      <h5 class="card-title">${list[i].symbol}Coin </h5>
+      <p class="card-text">${list[i].name}Coin Name</p>
+      <a href="#" class="btn btn-primary">More Info</a>
+      </div>  
+      </div>
+      </div>
+      `
+    )}
   }
-  
-  
-  loadCoinData()
-}})
 
-$('#load-coins').click(function (){
+$('#load-coins').click(function () {
 
   $.ajax({
-    url: `https://api.coingecko.com/api/v3/coins/list`,
-    success: data =>{
-      console.table(data)
+    url: (`https://api.coingecko.com/api/v3/coins/list`),
+    success: data => {
+      loadCoinData(data)
     }
   })
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
+
